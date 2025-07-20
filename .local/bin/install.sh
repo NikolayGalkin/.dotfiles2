1f6bc89310e1aca889103f92d53ea68718759888
Mykola Halkin <mykola.halkin@nixs.com>
2025-07-20 13:35:21 +0200

    init project

---
+++ .local/bin/install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+cd $HOME
+git clone --bare git@github.com:NikolayGalkin/.dotfiles2.git $HOME/.local/share/.dotfiles
+function dot {
+   /usr/bin/git --git-dir=$HOME/.local/share/.dotfiles --work-tree=$HOME $@
+}
+mkdir -p .backup
+dot checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+  dot checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .backup/{}
+fi;
+dot checkout
+dot config status.showUntrackedFiles no
+
+
+# install mise
+# curl https://mise.run | sh
